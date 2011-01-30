89e6cfe97a1d69d0aa375c41bed572819791caff
Jaime Bellmyer <online@bellmyer.com>
2011-01-30 09:52:04 -0600

    initial rails install with rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoubleBlindTesting::Application.config.secret_token = '1fa85fac55a396ae8163a86b3ee53e3f34ff5a50a549c46bd573958f9b2b7387fe6a2d4ad33fcd1913c5de7750de174ff89917df2fc2b4252856913aec06c73b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DoubleBlindTesting::Application.config.session_store :cookie_store, :key => '_double_blind_testing_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DoubleBlindTesting::Application.config.session_store :active_record_store
