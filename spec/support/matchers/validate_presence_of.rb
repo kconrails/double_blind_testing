7df2ce546fce4b90ce5d02604aa7835e0a2d3ac2
Jaime Bellmyer <online@bellmyer.com>
2011-01-31 01:56:23 -0600

    updated validate_presence_of to accept custom message

--- spec/support/matchers/validate_presence_of.rb
+++ spec/support/matchers/validate_presence_of.rb
@@ -2,31 +2,32 @@ RSpec::Matchers.define :validate_presence_of do |expected, options|
   match do |actual|
     object.send("#{expected}=", value(options))
     object.valid?
-    object.errors[expected].should_not include(error_message)
+    object.errors[expected].should_not include(error_message(options))
 
     object.send("#{expected}=", nil)
     object.should_not be_valid
-    object.errors[expected].should include(error_message)
+    object.errors[expected].should include(error_message(options))
 
     object.send("#{expected}=", '')
     object.should_not be_valid
-    object.errors[expected].should include(error_message)
+    object.errors[expected].should include(error_message(options))
   end
 
   failure_message_for_should do |actual|
-    %(expected #{object.class}'s #{expected} errors to contain "#{error_message}")
+    %(expected #{object.class}'s #{expected} errors to contain "#{error_message(options)}")
   end
 
   failure_message_for_should_not do |actual|
-    %(expected #{object.class}'s #{expected} errors not to contain "#{error_message}")
+    %(expected #{object.class}'s #{expected} errors not to contain "#{error_message(options)}")
   end
 
   description do
     "validate the presence of #{expected}"
   end
   
-  def error_message
-    "can't be blank"
+  def error_message options
+    return @error_message if defined?(@error_message)
+    @error_message = options.is_a?(Hash) && options.has_key?(:message) ? options[:message] : "can't be blank"
   end
   
   def object
