3b649a890b436da3abaffa8d12043516bacdbefe
Jaime Bellmyer <online@bellmyer.com>
2011-02-01 03:07:15 -0600

    rearranged tests by attribute

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe Course do
   it {should belong_to :student}
-  it {should belong_to :subject}
-  
   it {should validate_presence_of_association :student}
+
+  it {should belong_to :subject}  
   it {should validate_presence_of_association :subject}
 
   it {should set_default_for :grade_percentage, :to => 1.0}
--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -2,11 +2,10 @@ require 'spec_helper'
 
 describe Subject do
   it {should belong_to :teacher}
+  it {should validate_presence_of_association :teacher}
   
   it {should validate_presence_of :name}
   it {should validate_length_of :name, :maximum => 50, :message => "must be 50 characters or less"}
 
   it {should validate_presence_of :number}
-  
-  it {should validate_presence_of_association :teacher}
 end
